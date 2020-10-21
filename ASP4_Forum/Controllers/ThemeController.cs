cd2b9ece4193386143e465c3268df0b2733453de
alexanderig <alexander.ignatyuk@gmail.com>
2020-10-21 17:26:47 +0300

    added teheme controller with view added partial scripts adn styles

---
+++ ASP4_Forum/Controllers/ThemeController.cs
@@ -0,0 +1,40 @@
+﻿using ASP4_Forum.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Identity.Owin;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+
+namespace ASP4_Forum.Controllers
+{
+    public class ThemeController : Controller
+    {
+        ApplicationDbContext _context;
+
+        public ApplicationDbContext DBContext
+        {
+            get
+            {
+                return _context ?? HttpContext.GetOwinContext().Get<ApplicationDbContext>();
+            }
+            private set
+            {
+                _context = value;
+            }
+        }
+
+        // GET: Theme
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public async Task<ActionResult> Index(int id)
+        {
+            Theme theme = await DBContext.Themes.FindAsync(id);
+            return View(theme);
+        }
+    }
+}
